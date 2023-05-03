1c331c65375e36cbc232382b01c64c46b29b58b9
Sneh <snehricky@gmail.com>
2023-05-03 18:23:24 +0530

    testing git commands

---
+++ src/test/java/com/scalar/NotesManagement/NotesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalar.NotesManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
